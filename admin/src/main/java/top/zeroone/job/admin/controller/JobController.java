e2068934dcd8696b85c6c32913fc3f832ae06dd8
北风刮的不认真了 <geek@songyang.space>
2020-03-31 10:43:20 +0800

    feat(controller): 接收上报的接口

--- admin/src/main/java/top/zeroone/job/admin/controller/JobController.java
+++ admin/src/main/java/top/zeroone/job/admin/controller/JobController.java
@@ -1,12 +1,21 @@
 package top.zeroone.job.admin.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import top.zeroone.job.manager.model.TaskDescription;
+
+import java.util.List;
+
 /**
  * @author songyang
  */
 public class JobController {
 
 
-    public void reportListener() {
+
+    @PostMapping("reportTask/{id}")
+    public void reportTask(@RequestBody List<TaskDescription> taskDescriptions, @PathVariable String id) {
 
 
     }
